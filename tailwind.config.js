b0c2a92631d23382eb4abad55338598d68812ce7
cristiana-nwabudike <Cristiana.nwabudike@gmail.com>
2025-10-23 15:35:56 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4B3F72",
+        accent: "#F2C14E"
+      }
+    }
+  },
+  plugins: []
+};
